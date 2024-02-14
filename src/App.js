0f6019577fb88c0bc06b21a9635c4c74bf3bbe59
Nicholas Channg <nicholas.channg@gmail.com>
2024-02-14 02:15:50 -0500

    1.0
    Valentines day.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import happy from "./img/happy.JPG";
+import neutral from "./img/neutral.JPG";
+import sad from "./img/sad.JPG";
+
+import { React, useState } from "react";
+
+import "./App.css";
 
 function App() {
+  let [img, setImg] = useState(neutral);
+  let [yesSize, setYesSize] = useState(74.69);
+  let [message, setMessage] = useState("Will you be my Valentine?");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>{message}</h1>
+        <img src={img} alt="" />
+        <div className="buttons">
+          <button
+            className="yes"
+            style={{ width: yesSize + "px" }}
+            onClick={() => {
+              setImg(happy);
+              setMessage("YAAAAAAAAAAAAY!🍆👌");
+              setYesSize(74.69);
+            }}
+          >
+            😍YES😍
+          </button>
+          <button
+            className="no"
+            onClick={() => {
+              setImg(sad);
+              setYesSize(yesSize + 20);
+              setMessage("Please say yes 😢");
+            }}
+          >
+            👎No👎
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
